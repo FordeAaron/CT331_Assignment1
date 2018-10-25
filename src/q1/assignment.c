b5972588f448812aa7b7f4a7f8517077c29568ab
AaronF97 <aaronforde97@hotmail.com>
2018-10-25 19:53:43 +0100

    Q1 answered

--- src/q1/assignment.c
+++ src/q1/assignment.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
-int main(int arg, char* argc[]){
-  printf("Hello assignment1.\n");
-  return 0;
+int main(int arg, char* argc[])
+{
+
+	printf(" Size of int = %d\n", sizeof(int));
+	printf(" Size of int* = %d\n", sizeof(int*));
+	printf(" Size of long = %d\n", sizeof(long));
+	printf(" Size of double* = %d\n", sizeof(double*));
+	printf(" Size of char** = %d", sizeof(char**));
+
+	return 0;
 }
